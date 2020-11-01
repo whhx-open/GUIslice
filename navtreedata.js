37c33c46cc4e471415ebe555e839a1d2501a887f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-01 06:55:10 +0000

    Deploy ImpulseAdventure/GUIslice to github.com/ImpulseAdventure/GUIslice.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,46 @@
+var NAVTREE =
+[
+  [ "GUIslice", "index.html", [
+    [ "GUIslice library", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GUIslice_8c.html",
+"GUIslice__drv__m5stack_8h.html#a34a34f19b579e6261c652e1ee6badac5",
+"XGauge_8h.html",
+"XKeyPad__Num-setup_8c.html#a6700277dc39a73cbc6e6f63272f7d292",
+"XSpinner_8c.html#ad763d455a39cc2a90c38061ac3dd5a28",
+"group____ElemUpd__.html#gaabfedf0888172eb13d626c2b2d2e5d01",
+"group____IntCollectEvt__.html",
+"group____Int__.html#ga57ec171be68057ec44d4bb92f0c869b0ab8109817f821925576ae63f9bd6782d6",
+"group____Int__.html#gadb46d158c20e04b8e18aae17b55ca9a7",
+"structgslc__tsXGauge.html#aaa7d22a6e12d515236872b918034238c"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
